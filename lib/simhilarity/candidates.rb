a40b175f9f771ad7dbc0fab2316c54a6274806c9
Brendan Ribera <brendan.ribera@gmail.com>
2013-04-24 15:00:23 -0700

    Function exists in score.rb too.

--- lib/simhilarity/candidates.rb
+++ lib/simhilarity/candidates.rb
@@ -91,35 +91,5 @@ module Simhilarity
       candidates
     end
 
-    # walk candidates by score, pick winners
-    def winners(needles, candidates)
-      # calculate this first so we get a nice progress bar
-      veach("Scoring", candidates) { |i| i.score }
-
-      # score the candidates
-      candidates = candidates.sort_by { |i| -i.score }
-
-      # walk them, eliminate dups
-      seen = Set.new
-      winners = candidates.map do |i|
-        next if seen.include?(i.a) || seen.include?(i.b)
-        seen << i.a
-        seen << i.b
-        i
-      end.compact
-
-      # build map from needle => candidate...
-      needle_to_winner = { }
-      winners.each { |i| needle_to_winner[i.a] = i }
-
-      # so we can return in the original order
-      needles.map do |i|
-        if candidate = needle_to_winner[i]
-          [ i.opaque, candidate.b.opaque, candidate.score ]
-        else
-          [ i.opaque, nil, nil ]
-        end
-      end
-    end
   end
 end
